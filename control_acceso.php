ef9abb8414e78356f2479d08fcb3a1b66281e013
Natalia Garcia <ngs43@gcloud.ua.es>
2017-10-25 20:25:12 +0200

    sdfasdf

--- control_acceso.php
+++ control_acceso.php
@@ -4,22 +4,33 @@ $title = "Menú de usuario - Pickle";
 require_once("cabecera.inc");
 require_once("inicio.inc");
 
+ 	
 
+	$nombre = $GET["nombre"];
+ 	$psw = $GET["psw"];
+	echo "hola"; 	
+ 	if(($nombre=="natalia") && ($psw == "natalia")){
 
+ 		header('Location: usuario_registrado.php');
+ 	}
 
- 
-	session_start();	
-	$_SESSION["nombre"] = $_POST["nombre"];
-	$_SESSION["psw"] = $_POST["psw"];
-	$_SESSION['message'] = 'Usuario y/o contraseña incorrectos';
+ 	else{
+ 			
+ 		header('Location: inicio.php');
+ 	}
+
+	//session_start();	
+	//$_SESSION["nombre"] = $_POST["nombre"];
+	//$_SESSION["psw"] = $_POST["psw"];
+	//$_SESSION['message'] = 'Usuario y/o contraseña incorrectos';
 	
 	
-	if(($_POST['nombre']) == "natalia" && ($_POST['psw']) == "natalia" ){		
-		header('Location: usuario_registrado.php');
-	}else{			
-		header('Location: inicio.php');	
+	//if(($_POST['nombre']) == "natalia" && ($_POST['psw']) == "natalia" ){		
+	//	header('Location: usuario_registrado.php');
+	//}else{			
+	//	header('Location: inicio.php');	
 		
-	}
+	//}
 
 
 
