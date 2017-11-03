271516cb16498947f02de66e5fded261af8e5cb5
lFevers <sergiopatohumanes@gmail.com>
2017-11-03 10:29:58 +0100

    Cambios para arreglar que mantenga la sesion iniciada o no Y practica 7 terminada

--- cerrarsesion.php
+++ cerrarsesion.php
@@ -3,10 +3,8 @@ session_destroy();
 
    //redireccion a pagina principal
    //setcookie("fechaUltimaVisita"," ",time()-3600); //elimino cookie
-   //setcookie("nombre"," ",time()-3600); //elimino cookie
-   if(isset($_SESSION["remember"])){
-		setcookie("fechaUltimaVisita", date("d-m-y H:i:s"), (time() + 365 * 24 * 60 * 60));
-   }  
+   setcookie("nombre"," ",time()-3600); //elimino cookie
+   setcookie("fechaUltimaVisita"," ",time()-3600); //elimino cookie
    header("Location: inicio.php");
   
 
--- control_acceso.php
+++ control_acceso.php
@@ -15,18 +15,21 @@ require_once("inicio.inc");
 
 	foreach ($usuarios as $usuario) {
 		if(($_POST['nombre']) == $usuario[0] && ($_POST['psw']) == $usuario[1]  ){
-		
+			$_SESSION["remember"] = $_POST["remember"];
+			$_SESSION["nombre"] = $_POST["nombre"];
+			$_SESSION["psw"] = $_POST["psw"];
+			$_SESSION['message'] = 'Usuario y/o contraseña incorrectos';
+			
 			if(isset($_POST["remember"])){
-
-				$_SESSION["remember"] = $_POST["remember"];
-				$_SESSION["nombre"] = $_POST["nombre"];
-				$_SESSION["psw"] = $_POST["psw"];
-				$_SESSION['message'] = 'Usuario y/o contraseña incorrectos';
+				setcookie("nombre", $_POST["nombre"], (time() + 365 * 24 * 60 * 60));
 				setcookie('remember', $_SESSION["remember"], (time() + 365 * 24 * 60 * 60));
-
-				setcookie('nombre', $_POST["nombre"], (time() + 365 * 24 * 60 * 60));
-				setcookie('psw', $_POST["psw"], (time() + 365 * 24 * 60 * 60));
 				//setcookie("fechaUltimaVisita", date("d-m-y H:i:s"), (time() + 365 * 24 * 60 * 60));
+				setcookie("fechaUltimaVisita", date("d-m-y H:i:s"), (time() + 365 * 24 * 60 * 60));
+  				
+			}
+			else{
+				setcookie("nombre"," ",time()-3600); //elimino cookie
+				setcookie("fechaUltimaVisita"," ",time()-3600); //elimino cookie
 			}
 
 
