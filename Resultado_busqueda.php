61f3e675940d742dc3559921df8cfcd2c91fba21
Natalia Garcia <ngs43@gcloud.ua.es>
2017-11-27 12:46:51 +0100

    tu cosa bien

--- Resultado_busqueda.php
+++ Resultado_busqueda.php
@@ -16,56 +16,20 @@ require_once("inicio.inc");
 	exit;
 	} 
 
-	$entra1 = false;
-	$entra2 = false;
-	$entra3 = false;
 
+	$sentencia = 'SELECT * from fotos f , paises p where p.idPais = f.pais ';
+	
 	if(($_POST['titulo']) != ""){
 		echo "<b><p>Titulo:</b>" . $_POST["titulo"]. "</p>";
-		$entra1 = true;
+		$sentencia = $sentencia . 'and f.titulo like "%' . $_POST["titulo"] . '%"';
 	}
 	if(($_POST['fecha']) != ""){
 		echo "<b><p>Fecha:</b>" . $_POST["fecha"]. "</p>";
-		$entra2 = true;
+		$sentencia = $sentencia . 'and f.fRegistro like "%' . $_POST["fecha"] . '%"';
 	}
 	if(($_POST['pais']) != ""){
 		echo "<b><p>País:</b>" . $_POST["pais"] . "</p>";
-		$entra3 = true;
-	}
-
-
-	if ($entra1 == false && $entra2 == false && $entra3 == false){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where p.idPais = f.pais';
-	}
-
-	if ($entra1 == false && $entra2 == false && $entra3 == true){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where p.nomPais ="' . $_POST["pais"] . '" and p.idPais = f.pais and p.idPais = f.pais';
-	}
-
-	if ($entra1 == false && $entra2 == true && $entra3 == false){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where fecha ="' .  $_POST["fecha"] . '" and p.idPais = f.pais';
-	}
-
-	if ($entra1 == false && $entra2 == true && $entra3 == true){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where fecha ="' .  $_POST["fecha"] . '" and p.nomPais ="' . $_POST["pais"] . '" and p.idPais = f.pais';
-	}
-
-
-
-	if ($entra1 == true && $entra2 == false && $entra3 == false){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where titulo = "' . $_POST["titulo"] . '" and p.idPais = f.pais';
-	}
-
-	if ($entra1 == true && $entra2 == false && $entra3 == true){
-		$sentencia = 'SELECT distinct * from fotos f , paises p  where titulo = "' . $_POST["titulo"] . '" and p.nomPais ="' . $_POST["pais"] . '" and p.idPais = f.pais';
-	}
-
-	if ($entra1 == true && $entra2 == true && $entra3 == false){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where titulo = "' . $_POST["titulo"] . '" and fecha ="' . $_POST["fecha"] . '" and p.idPais = f.pais';
-	}
-
-	if ($entra1 == true && $entra2 == true && $entra3 == true){
-		$sentencia = 'SELECT distinct * from fotos f , paises p where titulo = "' . $_POST["titulo"] . ' and p.nomPais ="' . $_POST["pais"] . '" and p.idPais = f.pais';
+		$sentencia = $sentencia . 'and p.nomPais like "%' . $_POST["pais"] . '%"';
 	}
 
 	// Ejecuta una sentencia SQL
@@ -76,10 +40,16 @@ require_once("inicio.inc");
 
 	}
 
+	$numrows = mysqli_num_rows($resultado);
+	
+	if ($numrows == 0){
+		echo '<p>No se han encontrado coincidencias con los datos solicitados</p>';
+	}
+
 	while($fila = mysqli_fetch_assoc($resultado)) {
 		echo ' 
 		<figure class="foto">
-		<a href="./detalle_foto.php?id='. $fila['idFoto'].'"><img src="'. $fila['fichero'] .'" alt="imagen no encontrada" class="foto" /></a> 		
+		<a href="Detalle_foto.php?id=11"><img src="' . $fila['fichero'] . '" alt="imagen no encontrada" style="width:175px;height:175px;" ></a>  		
 		<p><strong>Título:</strong>'. $fila['titulo'] .'</p>
 		<p><strong>Fecha:</strong>'. $fila['fRegistro'] .'</p>
 		<p><strong>País:</strong>'. $fila['nomPais'] .'</p>
